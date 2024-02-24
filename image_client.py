5a217a80f699134d817d80fdba199515acc53db2
Brandon <lazeralien@gmail.com>
2024-02-24 15:40:07 -0700

    Incremental Progress- Update hard coding and send camera images instead of storage

--- image_client.py
+++ image_client.py
@@ -3,6 +3,21 @@ import grpc
 import argparse
 import os
 from PIL import Image # This is Pillow from Ubuntu Dockerfile
+import time
+import board
+import busio
+import adafruit_mlx90640
+
+# Camera Setup
+i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
+mlx = adafruit_mlx90640.MLX90640(i2c)
+print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
+
+ip_address = '192.168.1.10'
+
+# if using higher refresh rates yields a 'too many retries' exception,
+# try decreasing this value to work with certain pi/camera combinations
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
 # Change directory to Routes so we can import the protobufs
 current_directory = sys.path[0]
@@ -32,12 +47,13 @@ def read_images_from_folder(image_file_path):
                 image_data = image_file.read()
                 yield image_pb2.ImageData(data=image_data)
 
-def run(image_file_path, server_address='localhost', server_port=50051):
+def run(image_file_path, server_address=ip_address, server_port=50051):
     # Connect to the gRPC server
     with grpc.insecure_channel(f'{server_address}:{server_port}') as channel:
         # Create a stub (client)
         stub = generic_pb2_grpc.DBGenericStub(channel)
 
+        # TODO: Remove the temporary for loop that takes images. Replace with mlk images
         # Read and send data from the CSV file
         for image_data in read_images_from_folder(image_file_path): 
             serial_data = image_data.SerializeToString()
@@ -49,13 +65,32 @@ def run(image_file_path, server_address='localhost', server_port=50051):
                 protobufs=[anypb_msg] 
             )
             response = stub.Insert(request)
-            print(f"Server Response: {response.errs}")
+
+
+        frame = [0] * 768
+        counter= 0
+
+        while True:
+            try:
+                mlx.getFrame(frame)
+            except ValueError:
+                # these happen, no biggie - retry
+                continue
+            print(type(frame))
+            print(frame)
+            #TODO: Replace loop below with a protobuf send like comments below
+            for h in range(24):
+                for w in range(32):
+                    t = frame[h*32 + w]
+                    # print("%0.1f, " % t, end="") #Prints the list of pixels
+                print()
+
 
 if __name__ == '__main__':
     # Use argparse to handle command-line arguments
     parser = argparse.ArgumentParser(description='Image gRPC Client')
     parser.add_argument('image_file_path', help='Path to the images')
-    parser.add_argument('--address', default='localhost', help='Address of the gRPC server')
+    parser.add_argument('--address', default=ip_address, help='Address of the gRPC server')
     parser.add_argument('--port', type=int, default=50051, help='Port number for the gRPC server')
 
     args = parser.parse_args()
