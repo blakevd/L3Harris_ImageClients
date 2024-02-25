a20cb0dc3d47cd6a84a77ccc7932f7db31bc47ec
Brandon <lazeralien@gmail.com>
2024-02-25 00:17:52 -0700

    Returned to default to enable different ip addresses ex.  python image_client.py data --address 192.168.1.10 --port 50051

--- image_client.py
+++ image_client.py
@@ -9,7 +9,7 @@ import busio
 import adafruit_mlx90640
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 # Camera Setup
@@ -17,7 +17,6 @@ i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
 mlx = adafruit_mlx90640.MLX90640(i2c)
 print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 
-ip_address = '192.168.1.10'
 
 # if using higher refresh rates yields a 'too many retries' exception,
 # try decreasing this value to work with certain pi/camera combinations
@@ -51,7 +50,7 @@ def read_images_from_folder(image_file_path):
                 image_data = image_file.read()
                 yield image_pb2.ImageData(data=image_data)
 
-def run(image_file_path, server_address=ip_address, server_port=50051):
+def run(image_file_path, server_address='localhost', server_port=50051):
     # Connect to the gRPC server
     with grpc.insecure_channel(f'{server_address}:{server_port}') as channel:
         # Create a stub (client)
@@ -85,15 +84,15 @@ def run(image_file_path, server_address=ip_address, server_port=50051):
             for h in range(24):
                 for w in range(32):
                     t = frame[h*32 + w]
-                    # print("%0.1f, " % t, end="") #Prints the list of pixels
-                # print()
+                    print("%0.1f, " % t, end="") #Prints the list of pixels
+                print()
 
 
 if __name__ == '__main__':
     # Use argparse to handle command-line arguments
     parser = argparse.ArgumentParser(description='Image gRPC Client')
     parser.add_argument('image_file_path', help='Path to the images')
-    parser.add_argument('--address', default=ip_address, help='Address of the gRPC server')
+    parser.add_argument('--address', default='localhost', help='Address of the gRPC server')
     parser.add_argument('--port', type=int, default=50051, help='Port number for the gRPC server')
 
     args = parser.parse_args()
