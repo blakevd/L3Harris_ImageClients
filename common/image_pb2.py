d98778390e287a5bc798c91bc6f04add8e80e4f9
Brandon <lazeralien@gmail.com>
2024-02-28 17:23:40 -0700

    protobuf now includes incrementing int identifier so we can order images

--- common/image_pb2.py
+++ common/image_pb2.py
@@ -12,9 +12,10 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
+from google.protobuf import any_pb2 as google_dot_protobuf_dot_any__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bimage.proto\"\x19\n\tImageData\x12\x0c\n\x04\x64\x61ta\x18\x01 \x01(\x0c\x42\x0bZ\t../commonb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bimage.proto\x1a\x19google/protobuf/any.proto\"-\n\tImageData\x12\x12\n\nidentifier\x18\x01 \x01(\x05\x12\x0c\n\x04\x64\x61ta\x18\x02 \x01(\x0c\x42\x0bZ\t../commonb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -22,6 +23,6 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'image_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['DESCRIPTOR']._options = None
   _globals['DESCRIPTOR']._serialized_options = b'Z\t../common'
-  _globals['_IMAGEDATA']._serialized_start=15
-  _globals['_IMAGEDATA']._serialized_end=40
+  _globals['_IMAGEDATA']._serialized_start=42
+  _globals['_IMAGEDATA']._serialized_end=87
 # @@protoc_insertion_point(module_scope)
--- image_client.py
+++ image_client.py
@@ -13,7 +13,6 @@ import time
 
 logging.basicConfig(level=logging.INFO)
 
-
 # Camera Setup
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
 mlx = adafruit_mlx90640.MLX90640(i2c)
@@ -35,39 +34,24 @@ import image_pb2_grpc
 import generic_pb2
 import generic_pb2_grpc
 
-def get_value(value):
-    try:
-        return int(value)
-    except ValueError:
-        try:
-            return float(value)
-        except ValueError:
-            return value
-
 def read_images_from_folder(image_file_path):
     for file_name in os.listdir(image_file_path):
         if file_name.endswith('.png'):
             image_path = os.path.join(image_file_path, file_name)
             with open(image_path, 'rb') as image_file:
                 image_data = image_file.read()
-                yield image_pb2.ImageData(data=image_data)
+                yield image_data
 
 def run(image_file_path, server_address='localhost', server_port=50051):
+    global counter
+    counter = 1
     # Connect to the gRPC server
     with grpc.insecure_channel(f'{server_address}:{server_port}') as channel:
         # Create a stub (client)
         stub = generic_pb2_grpc.DBGenericStub(channel)
 
-        frame = [0] * 768
-        # Temperature range in Celsius (adjust according to your application)
-        min_temperature = 34  # Minimum temperature in Celsius (setting for human detection)
-        max_temperature = 39  # Maximum temperature in Celsius (setting for human detection)
-
-        # Grayscale intensity range
-        min_intensity = 0  # Corresponding to min_temperature
-        max_intensity = 255  # Corresponding to max_temperature
-
         while True:
+            frame = [0] * 768
             try:
                 mlx.getFrame(frame)
             except ValueError:
@@ -79,6 +63,12 @@ def run(image_file_path, server_address='localhost', server_port=50051):
             pixels = img.load()  # Create pixel map
             for h in range(24):
                 for w in range(32):
+                    # Temperature range in Celsius (adjust according to your application)
+                    min_temperature = 20  # Minimum temperature in Celsius (setting for human detection)
+                    max_temperature = 50  # Maximum temperature in Celsius (setting for human detection)
+                    # Grayscale intensity range
+                    min_intensity = 0  # Corresponding to min_temperature
+                    max_intensity = 255  # Corresponding to max_temperature
                     # Scale the temperature value to the range [0, 1]
                     temperature = (frame[h*32 + w] - min_temperature) / (max_temperature - min_temperature)
                     # Map the temperature value to the grayscale intensity range [0, 255]
@@ -87,32 +77,66 @@ def run(image_file_path, server_address='localhost', server_port=50051):
                     intensity = max(0, min(intensity, 255))
                     pixels[w, h] = intensity
 
-            # Generate file name
-            file_name = os.path.join("data", f"frame_{time.time()}.png")
+            # Get the current timestamp
+            timestamp_ms = int(time.time() * 1000)
 
             # Save the image as a PNG file
+            file_name = os.path.join("data", f"frame_{timestamp_ms}.png")
             img.save(file_name, format='PNG')
 
-            for image_data in read_images_from_folder(image_file_path): 
-                serial_data = image_data.SerializeToString()
-                logging.info(f"Image Type: {type(serial_data)} \nData: {serial_data}")
-                type_url = f"ImageData"
-                anypb_msg = any_pb2.Any(value=serial_data, type_url=type_url)
-                
-                request = generic_pb2.protobuf_insert_request(
-                    keyspace="imageKeyspace",
-                    protobufs=[anypb_msg] 
-                )
-                logging.info(f"\nWorking Type: {type(request)} \nRequest: {request}")
-                response = stub.Insert(request)
+            # Read the saved image data
+            with open(file_name, 'rb') as image_file:
+                image_data = image_file.read()
+
+            # Create an instance of the ImageData message
+            image_message = image_pb2.ImageData()
+            image_message.identifier = counter
+            counter += 1
+
+            image_message.data = image_data
+
+            # Serialize the ImageData message to bytes
+            serialized_image = image_message.SerializeToString()
+
+            # Create an Any message to hold the serialized ImageData message
+            any_message = any_pb2.Any(value=serialized_image, type_url="ImageData")
+
+            # Create a request to send to the server
+            request = generic_pb2.protobuf_insert_request(
+                keyspace="imageKeyspace",
+                protobufs=[any_message]
+            )
+
+            # Send the request to the server
+            response = stub.Insert(request)
+
+            # Handle the response as needed
+            print("Response:", response)
 
             # Clear out the folder after processing
-            for filename in os.listdir(image_file_path):
-                file_path = os.path.join(image_file_path, filename)
-                if os.path.isfile(file_path):
-                    os.remove(file_path)
+            os.remove(file_name)
 
+# Deletes the entire table in the database
+def dropTable(server_address='localhost', server_port=50051):
+    # Connect to the gRPC server
+    with grpc.insecure_channel(f'{server_address}:{server_port}') as channel:
+        # Create a stub (client) for the generic service
+        stub = generic_pb2_grpc.DBGenericStub(channel)
 
+        # Create a delete request
+        droptable_request = generic_pb2.protobuf_droptable_request(
+            keyspace="imagekeyspace",
+            table="imagedata"
+        )
+
+        # Send the delete request
+        response = stub.DropTable(droptable_request)
+        # Check if response.errs is not empty
+        handle_errors(response.errs)
+
+def handle_errors(errors):
+    if errors != []:
+        logging.info(f"Server Response: {errors}")
 
 if __name__ == '__main__':
     # Use argparse to handle command-line arguments
@@ -120,8 +144,14 @@ if __name__ == '__main__':
     parser.add_argument('image_file_path', help='Path to the images')
     parser.add_argument('--address', default='localhost', help='Address of the gRPC server')
     parser.add_argument('--port', type=int, default=50051, help='Port number for the gRPC server')
+    parser.add_argument('--action', choices=['run', 'deleteall'], help='Action to perform')
 
     args = parser.parse_args()
 
-    # Runs the program with the provided arguments
-    run(args.image_file_path, server_address=args.address, server_port=args.port)
+    if args.action == 'run':
+        run(args.image_file_path, server_address=args.address, server_port=args.port)
+    elif args.action == 'deleteall':
+        dropTable(server_address=args.address, server_port=args.port)
+    else:
+        print("Invalid action. Please specify either 'run' or 'deleteall'.")
+    
