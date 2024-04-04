a6989542ea55507a88eef3e7bef4f43716544f89
Brandon <lazeralien@gmail.com>
2024-04-04 10:45:09 -0600

    Updated identifier and added option for no thermal camera

--- image_client.py
+++ image_client.py
@@ -1,28 +1,26 @@
 import sys
 import grpc
 import argparse
-import os
 from PIL import Image # This is Pillow from Ubuntu Dockerfile
 import board
 import busio
 import adafruit_mlx90640
 import logging
-import io
 import time
-import struct
+import random
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 # Camera Setup
 # set frequency in boot/config.txt not in script
 i2c = busio.I2C(board.SCL, board.SDA)
 mlx = adafruit_mlx90640.MLX90640(i2c)
-print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
+logging.info(f"MLX addr detected on I2C {[hex(i) for i in mlx.serial_number]}")
 
 
 # if using higher refresh rates yields a 'too many retries' exception,
 # try decreasing this value to work with certain pi/camera combinations
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_4_HZ
 
 # Change directory to Routes so we can import the protobufs
 current_directory = sys.path[0]
@@ -35,17 +33,7 @@ import image_pb2_grpc
 import generic_pb2
 import generic_pb2_grpc
 
-def read_images_from_folder(image_file_path):
-    for file_name in os.listdir(image_file_path):
-        if file_name.endswith('.png'):
-            image_path = os.path.join(image_file_path, file_name)
-            with open(image_path, 'rb') as image_file:
-                image_data = image_file.read()
-                yield image_data
-
-def run(image_file_path, server_address='localhost', server_port=50051):
-    global counter
-    counter = 1
+def run(server_address='localhost', server_port=50051):
     # Connect to the gRPC server
     with grpc.insecure_channel(f'{server_address}:{server_port}') as channel:
         # Create a stub (client)
@@ -53,16 +41,23 @@ def run(image_file_path, server_address='localhost', server_port=50051):
 
         while True:
             frame = [0] * 768
+
+            # This aquires thermal image from the mlx90640
             try:
                 mlx.getFrame(frame)
             except ValueError:
                 # these happen, no biggie - retry
                 continue
 
+            # #  __________
+            # # Use this code to replace try block above if you don't have the thermal camera
+            # for f in range(len(frame)):
+            #     frame[f] = random.uniform(0.0,100.0)
+            # # __________
+
             # Create an instance of the ImageData message
             image_message = image_pb2.ImageData()
-            image_message.identifier = counter
-            counter += 1
+            image_message.identifier = time.time()
 
             # add list of floats into long string and round the floats to save space            
             frame_rounded = [round(n, 1) for n in frame]
@@ -84,7 +79,7 @@ def run(image_file_path, server_address='localhost', server_port=50051):
             response = stub.Insert(request)
 
             # Handle the response as needed
-            print("Response:", response)
+            logging.info(f'Request: {image_message.identifier}')
             time.sleep(1)
 
 def stringify_float_list(float_list, delimiter=','):
@@ -115,7 +110,6 @@ def handle_errors(errors):
 if __name__ == '__main__':
     # Use argparse to handle command-line arguments
     parser = argparse.ArgumentParser(description='Image gRPC Client')
-    parser.add_argument('image_file_path', help='Path to the images')
     parser.add_argument('--address', default='localhost', help='Address of the gRPC server')
     parser.add_argument('--port', type=int, default=50051, help='Port number for the gRPC server')
     parser.add_argument('--action', choices=['run', 'deleteall'], help='Action to perform')
@@ -123,7 +117,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.action == 'run':
-        run(args.image_file_path, server_address=args.address, server_port=args.port)
+        run(server_address=args.address, server_port=args.port)
     elif args.action == 'deleteall':
         dropTable(server_address=args.address, server_port=args.port)
     else:
