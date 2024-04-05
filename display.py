8b7359f5c17bd89bb116c0fd4e95840dd85e5396
Brandon Babcock <u0518496@utah.edu>
2024-04-05 18:00:25 +0000

    Made it prettier

--- display.py
+++ display.py
@@ -1,7 +1,3 @@
-## This code was inspired and is heavily modified from https://makersportal.com/blog/2020/6/8/high-resolution-thermal-camera-with-raspberry-pi-and-mlx90640
-## This has run in a virtual python enviroment that has dependencies
-## Raspberry pi i2c baudrate is set to 1Mbps
-
 import time
 import board
 import busio
@@ -10,9 +6,11 @@ import adafruit_mlx90640
 import matplotlib.pyplot as plt
 from scipy import ndimage
 
+plt.style.use('dark_background')
+
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)  # setup I2C
 mlx = adafruit_mlx90640.MLX90640(i2c)  # begin MLX90640 with I2C comm
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ  # set refresh rate
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_32_HZ  # set refresh rate
 mlx_shape = (24, 32)  # mlx90640 shape
 
 mlx_interp_val = 10  # interpolate # on each dimension
@@ -21,7 +19,8 @@ mlx_interp_shape = (mlx_shape[0] * mlx_interp_val, mlx_shape[1] * mlx_interp_val
 fig, ax = plt.subplots(figsize=(12, 9))  # start figure
 therm1 = ax.imshow(np.zeros(mlx_interp_shape), interpolation='none', cmap=plt.cm.inferno, vmin=25, vmax=45)  # preemptive image
 cbar = fig.colorbar(therm1)  # setup colorbar
-cbar_label = cbar.set_label('Temperature [$^{\circ}$C]', fontsize=14)  # colorbar label
+cbar_label = cbar.set_label('Temp [$^{\circ}$C]', fontsize=14)  # colorbar label
+plt.title("L3Harris Clinic Team - Thermal Image Client View", color='white', fontweight='bold', fontsize=20)  # Our Team Name
 
 frame = np.zeros(mlx_shape[0] * mlx_shape[1])  # 768 pts
 
@@ -43,5 +42,6 @@ def plot_update():
 while True:
     try:
         plot_update()  # update plot
+        # print(f"Time: {time.time()}")
     except ValueError:
         continue
