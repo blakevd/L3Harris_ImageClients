8816b56f5bdb066553523eff05a1515294af6003
Brandon Babcock <u0518496@utah.edu>
2024-04-05 20:52:53 +0000

    Enables easy switch between celsius and fahrenheit with boolean

--- display.py
+++ display.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from scipy import ndimage
 
 plt.style.use('dark_background')
+fahrenheit = True #Values displayed as fahrenheit. If false will display celsius
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)  # setup I2C
 mlx = adafruit_mlx90640.MLX90640(i2c)  # begin MLX90640 with I2C comm
@@ -19,14 +20,24 @@ mlx_interp_shape = (mlx_shape[0] * mlx_interp_val, mlx_shape[1] * mlx_interp_val
 fig, ax = plt.subplots(figsize=(12, 9))  # start figure
 therm1 = ax.imshow(np.zeros(mlx_interp_shape), interpolation='none', cmap=plt.cm.inferno, vmin=25, vmax=45)  # preemptive image
 cbar = fig.colorbar(therm1)  # setup colorbar
-cbar_label = cbar.set_label('Temp [$^{\circ}$C]', fontsize=14)  # colorbar label
+if fahrenheit:
+    cbar_label = cbar.set_label('Temp [$^{\circ}$F]', fontsize=14)  # colorbar label
+else:
+    cbar_label = cbar.set_label('Temp [$^{\circ}$C]', fontsize=14)  # colorbar label
+
 plt.title("L3Harris Clinic Team - Thermal Image Client View", color='white', fontweight='bold', fontsize=20)  # Our Team Name
 
 frame = np.zeros(mlx_shape[0] * mlx_shape[1])  # 768 pts
 
 def plot_update():
     mlx.getFrame(frame)  # read mlx90640
-    data_array = np.fliplr(np.reshape(frame, mlx_shape))  # reshape, flip data
+
+    if fahrenheit:
+        frame_fahrenheit = celsius_to_fahrenheit(frame)    
+        data_array = np.fliplr(np.reshape(frame_fahrenheit, mlx_shape))  # reshape, flip data
+    else:
+        data_array = np.fliplr(np.reshape(frame, mlx_shape))
+
     data_array = ndimage.zoom(data_array, mlx_interp_val)  # interpolate
     therm1.set_array(data_array)  # set data
     
@@ -38,6 +49,9 @@ def plot_update():
     cbar.set_ticklabels(['{:.1f}'.format(min_temp), '{:.1f}'.format((min_temp + max_temp) / 2), '{:.1f}'.format(max_temp)])
 
     plt.pause(0.001)  # required
+    
+def celsius_to_fahrenheit(value):
+    return value * 1.8 + 32
 
 while True:
     try:
